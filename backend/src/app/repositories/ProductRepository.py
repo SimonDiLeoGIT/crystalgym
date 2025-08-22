f33afbd5b7043d3698e24b572e9c107308817456
simon <saimondiley@gmail.com>
2025-08-22 14:53:47 +0000

    feat: build product repository and tests

---
+++ backend/src/app/repositories/ProductRepository.py
@@ -0,0 +1,75 @@
+from app import db
+from app.models.product import Product
+from app.utils.pagination import PaginationHelper
+
+class ProductRepository:
+
+  def __init__(self):
+    self.pagination = PaginationHelper()
+
+  def create_product(self, name, description, code, release_date, gender_id, category_id):
+    if db.session.query(Product).filter(Product.code == code).first():
+      return None, f"Product with code {code} already exists", 409
+    product = Product(name=name, code=code, description=description, release_date=release_date, gender_id=gender_id, category_id=category_id)
+    db.session.add(product)
+    db.session.commit()
+    return product.to_json(), "Product created successfully", 201
+
+  def get_products(self, page=1, per_page=10, sort_by='id', sort_order='asc', search='', gender_id=None, category_id=None):
+    query = Product.query
+    if search:
+      query = query.filter(Product.name.contains(search) | Product.description.contains(search) | Product.code.contains(search))
+    if gender_id:
+      query = query.filter(Product.gender_id == gender_id)
+    if category_id:
+      query = query.filter(Product.category_id == category_id)
+
+    if sort_order == 'asc':
+      query = query.order_by(getattr(Product, sort_by).asc())
+    else:
+      query = query.order_by(getattr(Product, sort_by).desc())
+
+    # Total items before pagination
+    total_items = query.count()
+    total_pages = (total_items + per_page - 1) // per_page  # Ceiling division
+
+    # Pagination
+    pagination_helper = PaginationHelper()
+    products = pagination_helper.generate_pagination(page, per_page, query)
+
+    # Metadata
+    pagination_data = pagination_helper.get_pagination_data(page, per_page, total_items, total_pages)
+
+    data = {
+      "products": [product.to_json() for product in products],
+      "pagination_data": pagination_data
+    }
+
+    return data, "Products retrieved successfully", 200
+  
+  def get_product_by_id(self, product_id):
+    product = db.session.query(Product).filter(Product.id == product_id).first()
+    if not product:
+      return None, "Product with id {product_id} not found", 404
+    return product.to_json(), "Product retrieved successfully", 200
+  
+  def update_product(self, product_id, name, description, code, release_date, gender_id, category_id):
+    product = db.session.query(Product).filter(Product.id == product_id).first()
+    if not product:
+      return None, "Product with id {product_id} not found", 404
+    product.name = name
+    product.description = description
+    product.code = code
+    product.release_date = release_date
+    product.gender_id = gender_id
+    product.category_id = category_id
+    db.session.commit()
+    return product.to_json(), "Product updated successfully", 200
+  
+  def delete_product(self, product_id):
+    product = db.session.query(Product).filter(Product.id == product_id).first()
+    if not product:
+      return None, "Product with id {product_id} not found", 404
+    db.session.delete(product)
+    db.session.commit()
+    return product.to_json(), "Product deleted successfully", 200
---
+++ backend/src/tests/repositories/ProductRepository_test.py
@@ -0,0 +1,127 @@
+import pytest
+from app.repositories.ProductRepository import ProductRepository
+from app import db
+from app.models.product import Product
+from datetime import datetime
+
+class TestProductRepository:
+  @pytest.fixture(scope='module')
+
+  def product_repository(self):
+    return ProductRepository()
+  
+  # Test create product returns the product with code 201
+  def test_create_product(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      data, message, code = product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      assert data['name'] == 'test'
+      assert data['description'] == 'test'
+      assert data['code'] == 'THM'
+      assert code == 201
+      db.session.query(Product).delete()
+
+  # Test create_product returns 409 if product with code already exists
+  def test_create_product_returns_404_if_product_with_code_already_exists(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      data, message, code = product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      assert code == 409
+      db.session.query(Product).delete()
+
+  # Test get_products returns an empty list if no products are found
+  def test_get_products_returns_an_empty_list_if_no_products_are_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      db.session.query(Product).delete()
+      data, message, code = product_repository.get_products()
+      assert len(data['products']) == 0
+      db.session.query(Product).delete()
+
+  # Test get_products returns a list of products if products are found
+  def test_get_products_returns_a_list_of_products_if_products_are_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      data, message, code = product_repository.get_products()
+      assert len(data['products']) == 1
+      assert code == 200
+      db.session.query(Product).delete()
+
+  # Test get_products returns a list of products if parameters are passed
+  def test_get_products_returns_a_list_of_products_if_parameters_are_passed(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      product_repository.create_product('test_1', 'test_1', 'THM1', release_date, 1, 1)
+      product_repository.create_product('test_2', 'test_2', 'THM2', release_date, 2, 1)
+      data, message, code = product_repository.get_products(page=1, per_page=2)
+      assert len(data['products']) == 2
+      assert code == 200
+      db.session.query(Product).delete()
+
+  # Test get_products returns a list of products if parameters and page are passed
+  def test_get_products_returns_a_list_of_products_if_parameters_and_page_are_passed(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      product_repository.create_product('test_1', 'test_1', 'THM1', release_date, 1, 1)
+      product_repository.create_product('test_2', 'test_2', 'THM2', release_date, 2, 1)
+      data, message, code = product_repository.get_products(page=2, per_page=2)
+      assert len(data['products']) == 1
+      assert code == 200
+      db.session.query(Product).delete()
+
+  # Test get_product_by_id returns 404 if there are no products found
+  def test_get_product_by_id_returns_404_if_no_products_are_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      db.session.query(Product).delete()
+      data, message, code = product_repository.get_product_by_id(1)
+      assert code == 404
+
+  # Test get_product_by_id returns the product if it is found
+  def test_get_product_by_id_returns_the_product_if_it_is_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      data, message, code = product_repository.get_product_by_id(1)
+      assert data['name'] == 'test'
+      assert data['description'] == 'test'
+      assert data['code'] == 'THM'
+      assert code == 200
+      db.session.query(Product).delete()
+  
+  # Test update_product returns 404 if the product is not found
+  def test_update_product_returns_404_if_the_product_is_not_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      db.session.query(Product).delete()
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      data, message, code = product_repository.update_product(1, 'test', 'test', 'THM', release_date, 1, 1)
+      assert code == 404
+
+  # Test update_product returns 200 and updates the product if the product is found
+  def test_update_product_returns_200_if_the_product_is_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      data, message, code = product_repository.update_product(1, 'test_1', 'test_1', 'THM1', release_date, 1, 1)
+      assert code == 200
+      assert data['name'] == 'test_1'
+      assert data['description'] == 'test_1'
+      assert data['code'] == 'THM1'
+      db.session.query(Product).delete()
+
+  # Test delete product returns 404 if the product is not found
+  def test_delete_product_returns_404_if_the_product_is_not_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      db.session.query(Product).delete()
+      data, message, code = product_repository.delete_product(1)
+      assert code == 404
+
+  # Test delete product returns 200 and deletes the product if the product is found
+  def test_delete_product_returns_200_if_the_product_is_found(self, test_client, product_repository):
+    with test_client.application.app_context():
+      release_date = datetime.strptime("22/08/2025", "%d/%m/%Y").date()
+      product_repository.create_product('test', 'test', 'THM', release_date, 1, 1)
+      data, message, code = product_repository.delete_product(1)
+      assert code == 200
